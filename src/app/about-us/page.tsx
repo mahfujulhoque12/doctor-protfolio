00ed4ab181e063c67a80da988ff3002c42c9a31b
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-03-17 14:47:54 +0600

    add-new-code

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,7 +3,13 @@ import Clinic from "@/components/ui/clinic/Clinic";
 import Progress from "@/components/ui/progress/Progress";
 import Team from "@/components/ui/team/Team";
 import React from "react";
+import { Metadata } from "next";
 
+
+export const metadata:Metadata = {
+  title: "About Us | Doctor",
+  description: "Discover our team, services, and history at Doctor's Best Clinic."
+}
 const page = () => {
   return (
     <div>
